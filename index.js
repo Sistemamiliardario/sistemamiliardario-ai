4f0b135af3afaa6b7b842b0a872fe4d447e3bb59
Sistemamiliardario <163718565+Sistemamiliardario@users.noreply.github.com>
2025-05-05 09:15:45 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Benvenuto in SistemaMiliardario.AI 🚀</h1>
-      <p>Qui inizierà il tuo percorso con AI + Supabase.</p>
-    </main>
-  )
-}
